2bbf0d9f5002f1110a8c52bc92c8ade74eb09185
chanakyavardhan <67908785+chanakyavardhan@users.noreply.github.com>
2022-02-01 02:15:04 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -10,29 +10,39 @@ telegram_message() {
 }
 
 # Clone the Sync Repo
-git clone $FOX_SYNC
-cd sync
+#git clone $FOX_SYNC
+#cd sync
 
 # Setup the Sync Branch
-if [ -z "$SYNC_BRANCH" ]; then
-    export SYNC_BRANCH=$(echo ${FOX_BRANCH} | cut -d_ -f2)
-fi
+#if [ -z "$SYNC_BRANCH" ]; then
+#    export SYNC_BRANCH=$(echo ${FOX_BRANCH} | cut -d_ -f2)
+#fi
 
 # Sync the Sources
-./orangefox_sync.sh --branch $SYNC_BRANCH --path "$SYNC_PATH" || { echo "ERROR: Failed to Sync OrangeFox Sources!" && exit 1; }
+#./orangefox_sync.sh --branch $SYNC_BRANCH --path "$SYNC_PATH" || { echo "ERROR: Failed to Sync OrangeFox Sources!" && exit 1; }
 
-# Change to the Source Directory
+# Change to the directory where we'll sync the source
+mkdir -p $SYNC_PATH
 cd $SYNC_PATH
 
-# Clone the theme if not already present
-if [ ! -d bootable/recovery/gui/theme ]; then
-git clone https://gitlab.com/OrangeFox/misc/theme.git bootable/recovery/gui/theme || { echo "ERROR: Failed to Clone the OrangeFox Theme!" && exit 1; }
-fi
+# Init repo
+repo init --depth=1 -u $MANIFEST -b $MANIFEST_BRANCH
 
-# Clone the Commonsys repo, only for fox_9.0
-if [ "$FOX_BRANCH" = "fox_9.0" ]; then
-git clone --depth=1 https://github.com/TeamWin/android_vendor_qcom_opensource_commonsys.git -b android-9.0 vendor/qcom/opensource/commonsys || { echo "WARNING: Failed to Clone the Commonsys Repo!"; }
-fi
+# Sync
+repo sync --force-sync -j$(nproc --all) --no-tags --no-clone-bundle
+
+## Change to the Source Directory
+#cd $SYNC_PATH
+
+## Clone the theme if not already present
+#if [ ! -d bootable/recovery/gui/theme ]; then
+#git clone https://gitlab.com/OrangeFox/misc/theme.git bootable/recovery/gui/theme || { echo "ERROR: Failed to Clone the OrangeFox Theme!" && exit 1; }
+#fi
+
+## Clone the Commonsys repo, only for fox_9.0
+#if [ "$FOX_BRANCH" = "fox_9.0" ]; then
+#git clone --depth=1 https://github.com/TeamWin/android_vendor_qcom_opensource_commonsys.git -b android-9.0 vendor/qcom/opensource/commonsys || { echo "WARNING: Failed to Clone the Commonsys Repo!"; }
+#fi
 
 # Clone Trees
 git clone $DT_LINK $DT_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
